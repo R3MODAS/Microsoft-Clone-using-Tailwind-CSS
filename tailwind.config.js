edea559d17385e9cb7d50847ffeb7f5ed626fc7c
Sharadindu Das <remodas7774@gmail.com>
2023-04-14 18:30:45 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    container:{
+      center: true,
+      padding: '1rem',
+      screens : {
+        lg: "1500px",
+        xl: "1500px",
+        "2xl": "1500px"
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
